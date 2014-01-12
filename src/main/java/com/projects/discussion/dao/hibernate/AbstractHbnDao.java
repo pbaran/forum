b6cadd7ca212e6b73dd6fcf24280800ec07bd0e0
Piotr Baran <admin@piotrus.net.pl>
2014-01-12 16:52:42 +0100

    implementing CRUD operations for domain class

--- src/main/java/com/projects/discussion/dao/hibernate/AbstractHbnDao.java
+++ src/main/java/com/projects/discussion/dao/hibernate/AbstractHbnDao.java
@@ -25,9 +25,15 @@
 package com.projects.discussion.dao.hibernate;
 
 import com.projects.discussion.dao.Dao;
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.Date;
+import java.util.List;
 import javax.inject.Inject;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * @author Piotr Baran <admin@piotrus.net.pl>
@@ -39,5 +45,55 @@ public abstract class AbstractHbnDao<T extends Object> implements Dao<T>{
     
     protected Session getSession() {
         return sessionFactory.getCurrentSession();
+    }
+    
+    @SuppressWarnings("unchecked")
+    private Class<T> getDomainClass() {
+        if (domainClass == null) {
+            ParameterizedType thisType = (ParameterizedType) getClass().getGenericSuperclass();
+            this.domainClass = (Class<T>) thisType.getActualTypeArguments()[0];
+        }
+        return domainClass;
+    }
+    
+    private String getDomainClassName() {
+        return getDomainClass().getName();
+    }
+    
+    public void create(T t) {
+        Method method = ReflectionUtils.findMethod(
+            getDomainClass(), "setDateCreated",
+            new Class[] {Date.class});
+        if (method != null) {
+            try {
+                method.invoke(t, new Date());
+            } catch (Exception e) {
+                /* ignore */
+            }
+            getSession().save(t);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T get(Serializable id) {
+        return (T) getSession().get(getDomainClass(), id);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T load(Serializable id) {
+        return (T) getSession().load(getDomainClass(), id);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<T> getAll(){
+        return getSession().createQuery("from " + getDomainClassName()).list();
+    }
+    
+    public void update(T t) {
+        getSession().update(t);
+    }
+    
+    public void delete(T t) {
+        getSession().delete(t);
     }
 }
